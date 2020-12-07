4da9844bcf225802be6447078bb216c3ff1088c3
M. Adnan Khalid <madnankhalid.bukc@gmail.com>
2020-12-07 23:44:46 +0500

    Setting up according to linting rules

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Namaz Tracker React App
  Any One can track their namaz using this application
+
+If git not ignore node module then run these command
+'''js
+{
+    git rm -r --cached tracker/node_modules
+    git commit -m "removing node_modules"
+}
+'''
--- tracker/src/pages/Login/index.jsx
+++ tracker/src/pages/Login/index.jsx
@@ -1,140 +1,133 @@
-import React, { useEffect } from "react";
+import Logo from '../../assets/img/logo-with-out-bg.png'
+import '../../App.css'
+import React, { useEffect } from 'react'
 import {
-  Container,
-  Row,
-  Col,
-  Card,
-  Image,
-  Form,
   Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  Image,
+  Row,
   Spinner,
-} from "react-bootstrap";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
-import { Redirect } from "react-router-dom";
-import Logo from "../../assets/img/logo-with-out-bg.png";
-import { loginUser } from "../../redux/actions/userAction";
-import PropTypes from "prop-types";
-// import * as Yup from "yup";
-import "../../App.css";
+} from 'react-bootstrap'
 
-// const validationSchema = Yup.object({
-//   email: Yup.string().email("Invalid Email Format").required("Required"),
-//   pass: Yup.string().length(6, "Invalid Password").required("Required!"),
-// });
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { bindActionCreators } from 'redux'
+import { loginUser } from '../../redux/actions/userAction'
+// Import * as Yup from "yup";
 
-function Login(props) {
+import PropTypes from 'prop-types'
+
+const Login = (props) => {
   const onSubmit = (e) => {
-    e.preventDefault();
-    let { target } = e;
-    let credential = {
-      email: target[0].value,
-      pass: target[1].value,
-    };
-    console.log(credential);
-    props.loginUser(credential);
-  };
+    e.preventDefault()
+    const { target } = e,
+      credential = {
+        email: target[0].value,
+        pass: target[1].value,
+      }
+    props.loginUser(credential)
+  }
 
   useEffect(() => {
     if (props.errors) {
-      console.trace(props.errors);
+      console.log('Hello')
     }
-  }, [props]);
+  }, [props])
 
   if (props.authenticated) {
-    console.log("Success");
-    return <Redirect to="/dashboard" />;
-  } else {
-    return (
-      <Container fluid className="welcome">
-        <Row style={{ height: "100px" }}></Row>
-        <Row>
-          <Col></Col>
-          <Col>
-            <Card>
-              <Card.Body className="text-center">
-                <Image src={Logo} style={{ width: "100px" }} rounded />
-                <Card.Title className="text-center tracker-logo-heading">
-                  Namaz Tracker
-                </Card.Title>
-              </Card.Body>
-
-              <Card.Body>
-                <Form onSubmit={(e) => onSubmit(e)}>
-                  <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control
-                      type="email"
-                      placeholder="Enter email"
-                      name="email"
-                    />
-                  </Form.Group>
-
-                  <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                      type="password"
-                      placeholder="Password"
-                      name="pass"
-                    />
-                  </Form.Group>
-
-                  <Form.Group>
-                    <Button
-                      variant="primary"
-                      type="submit"
-                      style={{ width: "100%" }}
-                    >
-                      <div className="d-flex justify-content-center align-items-center">
-                        {props.loading && (
-                          <Spinner animation="grow" size="sm" role="status" />
-                        )}
-                        <span style={{ paddingLeft: 5 }}>Login</span>
-                      </div>
-                    </Button>
-                  </Form.Group>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col></Col>
-        </Row>
-      </Container>
-    );
+    return <Redirect to="/dashboard" />
   }
+  return (
+    <Container fluid className="welcome">
+      <Row style={{ height: '100px' }}></Row>
+      <Row>
+        <Col></Col>
+        <Col>
+          <Card>
+            <Card.Body className="text-center">
+              <Image src={Logo} style={{ width: '100px' }} rounded />
+              <Card.Title className="text-center tracker-logo-heading">
+                Namaz Tracker
+              </Card.Title>
+            </Card.Body>
+            <Form onSubmit={onSubmit}>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  name="pass"
+                />
+              </Form.Group>
+
+              <Form.Group>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  style={{ width: '100%' }}
+                >
+                  <div className="d-flex justify-content-center align-items-center">
+                    {props.loading && (
+                      <Spinner animation="grow" size="sm" role="status" />
+                    )}
+                    <span style={{ paddingLeft: 5 }}>Login</span>
+                  </div>
+                </Button>
+              </Form.Group>
+            </Form>
+            <Card.Body></Card.Body>
+          </Card>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  )
 }
 
-Login.prototype = {
-  error: PropTypes.string,
+Login.propTypes = {
   authenticated: PropTypes.bool,
-};
+  loading: PropTypes.bool,
+  errors: PropTypes.string,
+  loginUser: PropTypes.func,
+}
 
 /**
  *
  * @param {Global State} state
  * bind state to the component props
  */
-function mapStateToProps(state) {
-  const { user, UI } = state;
+const mapStateToProps = (state) => {
+  const { user, UI } = state
   return {
     authenticated: user.authenticated,
-    loading: user.loading,
     errors: UI.errors,
-  };
+    loading: user.loading,
+  }
 }
-
 /**
  *
- * @param {Redux Actions} dispatch
+ * Param {Redux Actions} dispatch
  * bind actions to the component props
  */
-function mapDispatchToProps(dispatch) {
+const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(
     {
       loginUser,
     },
-    dispatch
-  );
+    dispatch,
+  )
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
